f0eefc749d37affad960aef6dee9525ca262a84d
moj93 <mohamedyousef.se@gmail.com>
2022-09-03 23:06:07 +0200

    my first graphQl api

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var express_graphql = require('express-graphql').graphqlHTTP;
+var { buildSchema } = require('graphql');
+
+
+
+// GraphQL schema
+var schema = buildSchema(`
+    type Query {
+        message: String,
+        key : String
+    }
+`);
+
+
+
+
+
+// Root resolver
+var root = {
+    message: () => 'Hello World!',
+    key: () => 'Mohamed yousef!'
+};
+
+
+
+
+
+// Create an express server and a GraphQL endpoint
+var app = express();
+app.use('/graphql',express_graphql({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+app.listen(4000, () => console.log('Express GraphQL Server Now Running On localhost:4000/graphql'));
